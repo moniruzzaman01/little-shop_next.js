047aeab70028744421b516f0dabc07c0877f1f65
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-08-03 23:54:49 +0600

    dynamic route created with static data

--- app/page.js
+++ app/page.js
@@ -1,13 +1,11 @@
 import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
 import GetInTouch from "@/components/GetInTouch";
-import Navbar from "@/components/Navbar";
 import Products from "@/components/Products";
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <Banner />
       <Products />
       <GetInTouch />
--- components/Products.js
+++ components/Products.js
@@ -7,11 +7,11 @@ export default function Products() {
         <div>
           <div className="relative delay-150 w-180px lg:w-[270px] h-[205px] lg:h-[310px] bg-[#f8f8f8] bg-[url('/iphone.jpg')] bg-cover bg-center transition-all duration-3000 ease-in-out transform"></div>
           <h2 className="text-sm lg:text-base mt-2">
-            <Link className="text-base font-bold" href="./productPage.html">
+            <Link className="text-base font-bold" href="/products/{id}">
               iPhone 9
             </Link>
             <span className="text-[#919090]">
-              <Link href="./category.html">(Smartphones)</Link>
+              <Link href="/category/:categoryName">(Smartphones)</Link>
             </span>
           </h2>
           <p className="text-[#919090] text-sm">
