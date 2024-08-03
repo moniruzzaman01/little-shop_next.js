2c0c780fd9e02af6218a4479c783f18e455e75f7
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-08-04 01:07:45 +0600

    data loaded from product object in home page

--- components/SingleProduct.js
+++ components/SingleProduct.js
@@ -1,24 +1,40 @@
+import Image from "next/image";
 import Link from "next/link";
 
-export default function SingleProduct() {
+export default function SingleProduct({ product }) {
+  const { title, category, brand, discountPercentage, price, thumbnail } =
+    product;
+
   return (
     <div>
-      <div className="relative delay-150 w-180px lg:w-full h-[205px] lg:h-[310px] bg-[#f8f8f8] bg-[url('/iphone.jpg')] bg-cover bg-center transition-all duration-3000 ease-in-out transform"></div>
-      <h2 className="text-sm lg:text-base mt-2">
-        <Link className="text-base font-bold" href="./productPage.html">
-          iPhone 9
-        </Link>
-        <span className="text-[#919090]">
-          <Link href="./category.html">(Smartphones)</Link>
-        </span>
-      </h2>
-      <p className="text-[#919090] text-sm">
-        An apple mobile which is nothing like apple
-      </p>
+      <div
+        className={`relative delay-150 w-180px lg:w-full h-[205px] lg:h-[310px] bg-[#f8f8f8] transition-all duration-3000 ease-in-out transform`}
+      >
+        <Image
+          src={thumbnail}
+          alt={`${title} thumbnail`}
+          height={500}
+          width={500}
+        />
+      </div>
+      <div className="pl-2">
+        <h2 className="text-sm lg:text-base mt-2 space-x-1">
+          <Link className="text-base font-bold" href="./productPage.html">
+            {brand}
+          </Link>
+          <span className="text-[#919090]">
+            <Link href="./category.html">({category})</Link>
+          </span>
+        </h2>
+        <p className="text-[#919090] text-sm">{title}</p>
 
-      <p className="text-rose-600 text-sm mt-4">
-        <span className="text-[#919090] line-through">$205.00</span> $195.00
-      </p>
+        <p className="text-rose-600 text-sm mt-2">
+          <span className="text-[#919090] line-through">
+            ${discountPercentage}
+          </span>{" "}
+          $1{price}
+        </p>
+      </div>
     </div>
   );
 }
