795f8d205053d3daa53ca806aa268dfe3915c0e5
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-08-03 23:35:30 +0600

    required components created using initial template

---
+++ components/Navbar.js
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between w-11/12 lg:w-10/12 max-w-7xl py-5 lg:py-6 mx-auto">
+      <div className="flex gap-5 items-center justify-between lg:w-8/12 text-[#1A1A1A]">
+        <div className="flex items-center gap-2">
+          <Image
+            src="./assets/svg/menu.svg"
+            className="lg:hidden w-5 h-5"
+            height={20}
+            width={20}
+            alt=""
+          />
+          <Link href="./index.html">
+            <Image
+              src="./assets/lws-logo-black.svg"
+              className="h-10"
+              height={20}
+              width={20}
+              alt=""
+            />
+          </Link>
+        </div>
+      </div>
+      <div className="flex items-center justify-end gap-2 lg:gap-5 lg:w-4/12">
+        <Image
+          src="./assets/svg/avatar.svg"
+          className="hidden lg:block w-[18px] h-[18px]"
+          height={20}
+          width={20}
+          alt="login acount avatar"
+        />
+        <Image
+          src="./assets/svg/shopping-Cart.svg"
+          className="block w-5 h-5"
+          height={20}
+          width={20}
+          alt="shopping cart icon"
+        />
+      </div>
+    </nav>
+  );
+}
---
+++ components/Products.js
@@ -0,0 +1,87 @@
+import Link from "next/link";
+
+export default function Products() {
+  return (
+    <section className="w-11/12 lg:w-10/12 max-w-7xl mx-auto py-10">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 lg:grid-cols-4 my-4 lg:my-10">
+        <div>
+          <div className="relative delay-150 w-180px lg:w-[270px] h-[205px] lg:h-[310px] bg-[#f8f8f8] bg-[url('./assets/products/iphone.jpg')] bg-cover bg-center transition-all duration-3000 ease-in-out transform"></div>
+          <h2 className="text-sm lg:text-base mt-2">
+            <Link className="text-base font-bold" href="./productPage.html">
+              iPhone 9
+            </Link>
+            <span className="text-[#919090]">
+              <Link href="./category.html">(Smartphones)</Link>
+            </span>
+          </h2>
+          <p className="text-[#919090] text-sm">
+            An apple mobile which is nothing like apple
+          </p>
+
+          <p className="text-rose-600 text-sm mt-4">
+            <span className="text-[#919090] line-through">$205.00</span> $195.00
+          </p>
+        </div>
+
+        <div>
+          <div className="relative delay-150 w-180px lg:w-[270px] h-[205px] lg:h-[310px] bg-[#f8f8f8] bg-[url('./assets/products/macbook-pro.png')] bg-cover bg-center transition-all duration-3000 ease-in-out transform"></div>
+          <h2 className="text-sm lg:text-base mt-2">
+            <Link className="text-base font-bold" href="./productPage.html">
+              MacBook Pro
+            </Link>
+            <span className="text-[#919090]">
+              <Link href="./category.html">(Laptops)</Link>
+            </span>
+          </h2>
+          <p className="text-[#919090] text-sm">
+            MacBook Pro 2021 with mini-LED display may launch between September,
+            November
+          </p>
+
+          <p className="text-rose-600 text-sm mt-4">
+            <span className="text-[#919090] line-through">$205.00</span> $195.00
+          </p>
+        </div>
+
+        <div>
+          <div className="relative delay-150 w-180px lg:w-[270px] h-[205px] lg:h-[310px] bg-[#f8f8f8] bg-[url('./assets/products/samsung-galaxy-book.jpg')] bg-cover bg-center transition-all duration-3000 ease-in-out transform"></div>
+          <h2 className="text-sm lg:text-base mt-2">
+            <Link className="text-base font-bold" href="./productPage.html">
+              Samsung Galaxy Book
+            </Link>
+            <span className="text-[#919090]">
+              <Link href="./category.html">(Laptops)</Link>
+            </span>
+          </h2>
+          <p className="text-[#919090] text-sm">
+            Samsung Galaxy Book S (2020) Laptop With Intel Lakefield Chip, 8GB
+            of RAM Launched
+          </p>
+
+          <p className="text-rose-600 text-sm mt-4">
+            <span className="text-[#919090] line-through">$205.00</span> $195.00
+          </p>
+        </div>
+
+        <div>
+          <div className="relative delay-150 w-180px lg:w-[270px] h-[205px] lg:h-[310px] bg-[#f8f8f8] bg-[url('./assets/products/perfume.jpg')] bg-cover bg-center transition-all duration-3000 ease-in-out transform"></div>
+          <h2 className="text-sm lg:text-base mt-2">
+            <Link className="text-base font-bold" href="./productPage.html">
+              Brown Perfume
+            </Link>
+            <span className="text-[#919090]">
+              <Link href="./category.html">(Fragrances)</Link>
+            </span>
+          </h2>
+          <p className="text-[#919090] text-sm">
+            Royal_Mirage Sport Brown Perfume for Men & Women - 120ml
+          </p>
+
+          <p className="text-rose-600 text-sm mt-4">
+            <span className="text-[#919090] line-through">$205.00</span> $195.00
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
