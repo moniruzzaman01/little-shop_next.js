3199c5851afd58cf78f423c969bd552e76f59a75
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-08-04 00:27:26 +0600

    category page layout issue fixed

--- app/category/[categoryName]/page.js
+++ app/category/[categoryName]/page.js
@@ -1,3 +1,16 @@
+import Products from "@/components/Products";
+import SingleProduct from "@/components/SingleProduct";
+
 export default function CategoryPage() {
-  return <div>CategoryPage</div>;
+  return (
+    <div class="sticky top-0 right-0 w-full lg:w-10/12 grid grid-cols-2 gap-4 lg:grid-cols-3 my-4 lg:my-10">
+      <SingleProduct />
+      <SingleProduct />
+      <SingleProduct />
+      <SingleProduct />
+      <SingleProduct />
+      <SingleProduct />
+      <SingleProduct />
+    </div>
+  );
 }
--- app/category/layout.js
+++ app/category/layout.js
@@ -1,12 +1,16 @@
 import Banner from "@/components/Banner";
 import Footer from "@/components/Footer";
 import GetInTouch from "@/components/GetInTouch";
+import Sidebar from "@/components/Sidebar";
 
 export default function CategoryLayout({ children }) {
   return (
     <div>
       <Banner />
-      {children}
+      <section class="w-11/12 lg:w-10/12 max-w-7xl mx-auto py-0 lg:py-10 lg:flex justify-between items-start">
+        <Sidebar />
+        {children}
+      </section>
       <GetInTouch />
       <Footer />
     </div>
