44bdd15ae222cb1c62aded0b47db0bd293a6df1c
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-08-04 12:54:19 +0600

    make sidebar clickable

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,24 +1,38 @@
+"use client";
+
+import Link from "next/link";
+
 export default function Sidebar() {
   return (
-    <div className="w-full flex items-center justify-between lg:block lg:w-2/12 my-10 lg:my-0 lg:mt-4">
-      <button className="hover:border-b border-black block h-6 box-border mt-4">
+    <div className="w-full flex items-center flex-col lg:w-2/12 my-10 lg:my-0 lg:mt-4">
+      <Link
+        href={"/category/all"}
+        scroll={false}
+        className="hover:border-b border-black h-6 mt-4"
+      >
         All
-      </button>
-      <button className="hover:border-b border-black block h-6 box-border mt-5">
-        Smartphones
-      </button>
-      <button className="hover:border-b border-black block h-6 box-border mt-5">
-        Laptops
-      </button>
-      <button className="hover:border-b border-black block h-6 box-border mt-5">
-        Fragrances
-      </button>
-      <button className="hover:border-b border-black block h-6 box-border mt-5">
-        Skincare
-      </button>
-      <button className="hover:border-b border-black block h-6 box-border mt-5">
+      </Link>
+      <Link
+        href={"/category/beauty"}
+        scroll={false}
+        className="hover:border-b border-black h-6 mt-5"
+      >
+        Beauty
+      </Link>
+      <Link
+        href={"/category/fragnances"}
+        scroll={false}
+        className="hover:border-b border-black h-6 mt-5"
+      >
+        Fragnances
+      </Link>
+      <Link
+        href={"/category/groceries"}
+        scroll={false}
+        className="hover:border-b border-black h-6 mt-5"
+      >
         Groceries
-      </button>
+      </Link>
     </div>
   );
 }
