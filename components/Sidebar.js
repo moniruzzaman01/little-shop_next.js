854df9dcc280fa4ca51faa403acecc3e289b7d10
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2024-08-04 13:03:40 +0600

    active link feature added

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,35 +1,47 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Sidebar() {
+  const path = usePathname().split("/")[2];
+  console.log("path", path);
+
   return (
     <div className="w-full flex items-center flex-col lg:w-2/12 my-10 lg:my-0 lg:mt-4">
       <Link
         href={"/category/all"}
         scroll={false}
-        className="hover:border-b border-black h-6 mt-4"
+        className={`hover:border-b border-black h-6 mt-4 ${
+          path == "all" && "border-b"
+        }`}
       >
         All
       </Link>
       <Link
         href={"/category/beauty"}
         scroll={false}
-        className="hover:border-b border-black h-6 mt-5"
+        className={`hover:border-b border-black h-6 mt-5 ${
+          path == "beauty" && "border-b"
+        }`}
       >
         Beauty
       </Link>
       <Link
         href={"/category/fragnances"}
         scroll={false}
-        className="hover:border-b border-black h-6 mt-5"
+        className={`hover:border-b border-black h-6 mt-5 ${
+          path == "fragnances" && "border-b"
+        }`}
       >
         Fragnances
       </Link>
       <Link
         href={"/category/groceries"}
         scroll={false}
-        className="hover:border-b border-black h-6 mt-5"
+        className={`hover:border-b border-black h-6 mt-5 ${
+          path == "groceries" && "border-b"
+        }`}
       >
         Groceries
       </Link>
